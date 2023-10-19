4ab48b79c0057f20f630e609ac0103319d9bfe77
Jussi Kinnunen <Jussi.Kinnunen@wunderdog.fi>
2023-10-19 22:37:09 +0300

    made watchdog that resets if nothing comes

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
+#include <Ticker.h>
 
 enum State {NORMAL, GREENHOUSE1, GREENHOUSE2};
 
@@ -13,7 +14,9 @@ State state = NORMAL;
 int noMessagesIn = 0;
 int waterTics = 0;
 int loops=0;
+int rupts = 0;
 boolean overflow = false;
+Ticker watchdogTimer;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -22,47 +25,21 @@ void setState(State newState) {
   state = newState;
   switch (state) {
     case State::GREENHOUSE1:
-      digitalWrite(BYPASS, HIGH);
-      digitalWrite(GREENHOUSE1, HIGH);
-      digitalWrite(GREENHOUSE2, LOW);
+      digitalWrite(BYPASS, LOW);
+      digitalWrite(GREENHOUSE1PIN, LOW);
+      digitalWrite(GREENHOUSE2PIN, HIGH);
       break;
     case State::GREENHOUSE2:
-      digitalWrite(BYPASS, HIGH);
-      digitalWrite(GREENHOUSE1, LOW);
-      digitalWrite(GREENHOUSE2, HIGH);
+      digitalWrite(BYPASS, LOW);
+      digitalWrite(GREENHOUSE1PIN, HIGH);
+      digitalWrite(GREENHOUSE2PIN, LOW);
       break;
     default:
-      digitalWrite(BYPASS, LOW);
-      digitalWrite(GREENHOUSE1, LOW);
-      digitalWrite(GREENHOUSE2, LOW);
+      digitalWrite(BYPASS, HIGH);
+      digitalWrite(GREENHOUSE1PIN, HIGH);
+      digitalWrite(GREENHOUSE2PIN, HIGH);
       break;
   }
-}
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-
-   String topicStr = String(topic);
-   String payloadStr = "";
-   for (int i = 0; i < length; i++) {
-    payloadStr += (char)payload[i];
-   }
-    Serial.print(payloadStr);
-    Serial.println("] ");
-    if(topicStr == COMMAND_TOPIC){
-      if(payloadStr == "greenhouse1"){
-        setState(State::GREENHOUSE1);  
-      }else if(payloadStr == "greenhouse2"){
-        setState(State::GREENHOUSE2);
-      }else if(payloadStr == "normal"){
-        setState(State::NORMAL);
-      }}
-   noMessagesIn=0;
-}
-
-void IRAM_ATTR reactToWaterTick(){
-  waterTics++;
 }
 
 String stateToString(State state) {
@@ -79,20 +56,56 @@ String stateToString(State state) {
 }
 
 void sendStatus() {
+  Serial.println("sending status");
   StaticJsonDocument<200> doc;
   doc["state"] = stateToString(state);
   doc["waterTics"] = waterTics;
   doc["overflow"] = overflow;
+  doc["loops"] = loops;
   String jsonStr;
   serializeJson(doc, jsonStr);
   client.publish(STATUS_TOPIC, jsonStr.c_str());
 }
 
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  rupts=0;
+   String topicStr = String(topic);
+   String payloadStr = "";
+   for (int i = 0; i < length; i++) {
+    payloadStr += (char)payload[i];
+   }
+    Serial.print(":");
+    Serial.print(payloadStr);
+    Serial.println("] ");
+    if(topicStr == COMMAND_TOPIC){
+      if(payloadStr == "greenhouse1"){
+        setState(State::GREENHOUSE1); 
+        sendStatus(); 
+      }else if(payloadStr == "greenhouse2"){
+        setState(State::GREENHOUSE2);
+        sendStatus();
+      }else if(payloadStr == "normal"){
+        setState(State::NORMAL);
+        sendStatus();
+      }}
+   noMessagesIn=0;
+}
+
+void IRAM_ATTR reactToWaterTick(){
+  waterTics++;
+}
+
+
+
+
+
 void setupWifi(){
     WiFi.begin(WIFIHOTSPOT, WIFIKEY);
     while (WiFi.status() != WL_CONNECTED) {
       delay(500);
-      Serial.print(".");
+      Serial.print("waitingWifiß");
     }
     client.setServer(MQTT_SERVER, 1883);
     client.setCallback(callback);
@@ -103,7 +116,7 @@ void reconnect() {
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Create a random client ID
-    String clientId = "ESP8266Client-";
+    String clientId = "WaterDirector-";
     clientId += String(random(0xffff), HEX);
     // Attempt to connect
     if (client.connect(clientId.c_str())) {
@@ -124,6 +137,12 @@ void reconnect() {
     sendStatus();
   }
 
+  void seeThatWeAreStillRunning(){
+    rupts++;
+    if(rupts>12)
+      ESP.reset();
+  }
+
 
   void setup() {
     Serial.begin(9600);
@@ -134,10 +153,13 @@ void reconnect() {
     pinMode(GREENHOUSE2PIN, OUTPUT);
     pinMode(OVERFLOW, INPUT_PULLUP);
     Serial.println("Setup-Wifi");
+    watchdogTimer.attach(10,seeThatWeAreStillRunning);
     setupWifi();
     Serial.println("Setup-Interrupts");
     attachInterrupt(digitalPinToInterrupt(WATER_PIN), reactToWaterTick, FALLING);
     attachInterrupt(digitalPinToInterrupt(OVERFLOW), reactToOverflowChange, CHANGE);
+    setState(State::NORMAL);
+
   }
 
   
@@ -149,11 +171,13 @@ void loop() {
   client.loop();
   delay(300);
   client.loop();
-  loops++;
-  if(loops%10==0){
+  if(loops%15==0){
+    Serial.println("trying sending status");
     sendStatus();
   }
-  Serial.println(".");
+  Serial.println("loop");
   delay(300);
   client.loop();
+  overflow = !digitalRead(OVERFLOW);
+  loops++;
 }
